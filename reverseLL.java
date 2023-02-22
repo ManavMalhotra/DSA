9cff34cd9b7d19b1e7bbea10915ba7891d5d3d3d
ManavMalhotra <manavmalhotrafrnd4u@gmail.com>
2023-02-22 22:43:08 +0530

    Reverse a Linked List Iteratively

---
+++ reverseLL.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        
+        ListNode prev = null;        
+        ListNode curr = head;
+        
+        while(curr!=null){
+            ListNode nextNode = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+        
+        return prev;
+
+        
+    }
+}
