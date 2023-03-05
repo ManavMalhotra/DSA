bf683d16c6b95a3238dc3c74dc3b02fe9b50462e
ManavMalhotra <manavmalhotrafrnd4u@gmail.com>
2023-03-05 22:11:23 +0530

    Binary Search Search Insert Position

---
+++ searchInsert.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length-1;
+        
+        while(left<=right){
+            
+            int mid = (left + right)/2;
+            
+            if(target == nums[mid]){
+                return mid;
+            }else if(nums[mid] > target){
+                right = mid - 1 ;
+            }else{
+                left = mid + 1;
+            }
+        }
+    
+        return left;
+    }
+}
