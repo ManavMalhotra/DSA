736bc6b3a0b20404589eb8f483cdde11028a9320
Manav Malhotra <64147782+ManavMalhotra@users.noreply.github.com>
2023-08-01 21:48:23 +0530

    Time: 2 ms (91.53%), Space: 40.5 MB (66.13%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,30 @@
+
+import java.util.Stack;
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        
+        for(int i = 0; i < s.length(); i++){
+            char ch = s.charAt(i);
+            
+            if(ch == '(' || ch == '[' || ch == '{'){
+                stack.push(ch);
+            }else {
+                if(stack.isEmpty()){
+                    return false;
+                }
+                char top = stack.peek();
+                
+                if((ch == ')' && top == '(') || (ch == ']' && top == '[') || (ch == '}' && top == '{')){
+                    stack.pop();
+                }else {
+                    return false; 
+                }
+
+            }
+
+        }
+        return stack.isEmpty();
+    }
+}
+// @lc code=end
