5745d0eb79bd939387d69cfeec209ddc4637569c
Manav Malhotra <64147782+ManavMalhotra@users.noreply.github.com>
2023-08-02 18:36:09 +0530

    Time: 31 ms (6.83%), Space: 43.8 MB (28.24%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        Map<Character, Integer> countS = new HashMap<>();
+        Map<Character, Integer> countT= new HashMap<>();
+        
+        if(s.length() != t.length()){
+            return false;
+        }
+        
+        for(int i = 0; i< s.length(); i++){
+            
+            if (countS.containsKey(s.charAt(i))){
+                countS.put( s.charAt(i), countS.get(s.charAt(i)) + 1);
+            } else{
+                countS.put( s.charAt(i), 1);
+            }
+            
+            if (countT.containsKey(t.charAt(i))){
+                countT.put( t.charAt(i), countT.get(t.charAt(i)) + 1);
+            } else{
+                countT.put( t.charAt(i), 1);
+            }
+            
+        }
+        
+        for(char k: countS.keySet()){
+            if(!countS.get(k).equals(countT.get(k))) return false;
+        }
+        
+        return true;
+
+    }
+}
