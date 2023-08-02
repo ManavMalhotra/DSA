0dfb05325605f3e30026510379b7bf6f253a8f04
Manav Malhotra <64147782+ManavMalhotra@users.noreply.github.com>
2023-08-02 18:45:36 +0530

    Time: 46 ms (5.07%), Space: 44.8 MB (5.44%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,33 +1,41 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        Map<Character, Integer> countS = new HashMap<>();
-        Map<Character, Integer> countT= new HashMap<>();
         
-        if(s.length() != t.length()){
-            return false;
-        }
-        
-        for(int i = 0; i< s.length(); i++){
-            
-            if (countS.containsKey(s.charAt(i))){
-                countS.put( s.charAt(i), countS.get(s.charAt(i)) + 1);
-            } else{
-                countS.put( s.charAt(i), 1);
-            }
-            
-            if (countT.containsKey(t.charAt(i))){
-                countT.put( t.charAt(i), countT.get(t.charAt(i)) + 1);
-            } else{
-                countT.put( t.charAt(i), 1);
-            }
-            
-        }
-        
-        for(char k: countS.keySet()){
-            if(!countS.get(k).equals(countT.get(k))) return false;
-        }
-        
-        return true;
-
+       return getSortedString(s).equals(getSortedString(t));
     }
+   
+   private String getSortedString(String s) {
+       return s.chars().mapToObj(c -> (char)c).sorted().map(String::valueOf).collect(Collectors.joining());
+   }
+
+//         Map<Character, Integer> countS = new HashMap<>();
+//         Map<Character, Integer> countT= new HashMap<>();
+        
+//         if(s.length() != t.length()){
+//             return false;
+//         }
+        
+//         for(int i = 0; i< s.length(); i++){
+            
+//             if (countS.containsKey(s.charAt(i))){
+//                 countS.put( s.charAt(i), countS.get(s.charAt(i)) + 1);
+//             } else{
+//                 countS.put( s.charAt(i), 1);
+//             }
+            
+//             if (countT.containsKey(t.charAt(i))){
+//                 countT.put( t.charAt(i), countT.get(t.charAt(i)) + 1);
+//             } else{
+//                 countT.put( t.charAt(i), 1);
+//             }
+            
+//         }
+        
+//         for(char k: countS.keySet()){
+//             if(!countS.get(k).equals(countT.get(k))) return false;
+//         }
+        
+        // return true;
+
+    
 }
