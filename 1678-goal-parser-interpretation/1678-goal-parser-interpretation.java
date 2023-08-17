09dea855f6e7659f3e05ea5c3f00b2eb20a7a493
Manav Malhotra <64147782+ManavMalhotra@users.noreply.github.com>
2023-08-17 23:28:50 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (98.51%) - LeetHub

---
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public String interpret(String command) {
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 0; i < command.length(); i++) {
+            if (command.charAt(i) == 'G') {
+                sb.append('G');
+            } else if ((command.charAt(i) == '(') && (command.charAt(i + 1) == ')')) {
+                sb.append('o');
+            } else if ((command.charAt(i) == '(') && (command.charAt(i + 1) == 'a') && (command.charAt(i + 2) == 'l')
+                    && (command.charAt(i + 3) == ')')) {
+                sb.append('a');
+                sb.append('l');
+                i += 3;
+            }
+        }
+        
+        return sb.toString();
+    }
+}
